2e3d6ab4ec9b0f634e25fb8fced573967cc6ba85
StaryFritz <mihail.tennis@gmail.com>
2021-12-27 11:30:59 +0300

    lab14

---
+++ lab14/lab14/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace lab14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tvShows = new List<TVShow>();
+            string[] names = new string[] { "The Sopranos", "The Office", "The Simpsons", "The Witcher", "Friends" };
+            for (int i = 0; i < names.Length; i++)
+                tvShows.Add(new TVShow(names[i]));
+            var binFormatter = new BinaryFormatter();
+            using (var file = new FileStream("tvShows.bin", FileMode.Create))
+            {
+                binFormatter.Serialize(file, tvShows);
+            }
+            using (var file = new FileStream("tvShows.bin", FileMode.Open))
+            {
+                var newTVShows = binFormatter.Deserialize(file) as List<TVShow>;
+                if (newTVShows != null)
+                    foreach (var item in newTVShows)
+                        Console.WriteLine(item);
+            }
+            Console.ReadLine();
+            var soapFormatter = new SoapFormatter();
+            using (var file = new FileStream("tvShows.soap", FileMode.Create))
+            {
+                soapFormatter.Serialize(file, tvShows);
+            }
+            using (var file = new FileStream("tvShows.soap", FileMode.Open))
+            {
+                var newTVShows = soapFormatter.Deserialize(file) as List<TVShow>;
+                if (newTVShows != null)
+                    foreach (var item in newTVShows)
+                        Console.WriteLine(item);
+            }
+            Console.ReadLine();
+            var xmlFormatter = new XmlSerializer(typeof(List<TVShow>));
+            using (var file = new FileStream("tvShows.xml", FileMode.Create))
+            {
+                xmlFormatter.Serialize(file, tvShows);
+            }
+            using (var file = new FileStream("tvShows.xml", FileMode.Open))
+            {
+                var newTVShows = xmlFormatter.Deserialize(file) as List<TVShow>;
+                if (newTVShows != null)
+                    foreach (var item in newTVShows)
+                        Console.WriteLine(item);
+            }
+            Console.ReadLine();
+            var jsonFormatter = new DataContractJsonSerializer(typeof(List<TVShow>));
+            using (var file = new FileStream("tvShows.json", FileMode.Create))
+            {
+                jsonFormatter.WriteObject(file, tvShows);
+            }
+            using (var file = new FileStream("tvShows.json", FileMode.Open))
+            {
+                var newTVShows = jsonFormatter.ReadObject(file) as List<TVShow>;
+                if (newTVShows != null)
+                    foreach (var item in newTVShows)
+                        Console.WriteLine(item);
+            }
+
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Load(@"C:\Users\Михаил Лагуновский\Desktop\ООТП\lab14\lab14\bin\Debug\net5.0\tvShows.xml");
+            var xRoot = xmlDocument.DocumentElement;
+
+            var selectNodes = xRoot.SelectNodes("*");
+            foreach (object node in selectNodes) Console.WriteLine((node as XmlNode).Name);
+
+            Console.WriteLine();
+            var nameNodes = xRoot.SelectNodes("TVShow");
+            foreach (XmlNode n in nameNodes)
+                Console.WriteLine(n.SelectSingleNode("Name").InnerText);
+        }
+    }
+}
---
+++ lab14/lab14/TVShow.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab14
+{
+    [Serializable]
+    public class TVShow
+    {
+        [NonSerialized]
+        Random rnd = new Random();
+        public string Name { get; set; }
+        private int Length { get; set; }
+        public TVShow()
+        {
+            Name = "Some TVShow";
+            Length = rnd.Next(30, 180);
+        }
+        public TVShow(string name)
+        {
+            Name = name;
+            Length = rnd.Next(30, 180);
+        }
+        public override string ToString()
+        {
+            return $"It's a {Name} which last for - {Length}";
+        }
+    }
+    [Serializable]
+    public class Movie : TVShow
+    {
+        public string Genre { get; set; }
+        public Movie()
+        {
+            Name = "Movie";
+        }
+        public Movie(string name, int length, string genre) : base(name)
+        {
+            Genre = genre;
+        }
+        public override string ToString()
+        {
+            return $"It's {this.GetType()}, name - {this.Name}";
+        }
+        public override bool Equals(object obj)
+        {
+            Movie someMovie = (Movie)obj;
+            return (Name == someMovie.Name && Genre == someMovie.Genre);
+        }
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+    }
+}
